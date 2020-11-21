f6e9ff40b4709c55a09843e825d814acc363c18f
Marc Ferraro <marc.ferraro1@gmail.com>
2020-11-21 01:37:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -67,9 +67,9 @@ monopoly =
     },
     names: {
       reading_railroad: {mortgage_value: 100},
-      pennsylvania_railroad: {},
-      b_and_o_railroad: {},
-      shortline_railroad: {}
+      pennsylvania_railroad: {mortgage_value: 200},
+      b_and_o_railroad: {mortgage_value: 400},
+      shortline_railroad: {mortgage_value: 800}
     }
     
   }
